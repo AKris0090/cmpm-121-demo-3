d6c25895def86a79080b5c552453f556e6d4837a
Arjun Krishnan <akrish29@ucsc.edu>
2024-11-04 14:15:29 -0800

    Map with player marker and tiles complete

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,79 @@
-// todo
-const button = document.createElement("button");
-button.innerHTML = "Click me";
+import leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+import "./leafletWorkaround.ts";
+import luck from "./luck.ts";
 
-document.body.appendChild(button);
+const OAKES_CLASSROOM_POSITION = leaflet.latLng(
+  36.98949379578401,
+  -122.06277128548504,
+);
+const ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const MAX_COINS = 100;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
-button.onclick = () => {
-  alert("you clicked the button!");
-};
+interface Cache {
+  x: number;
+  y: number;
+  value: number;
+}
+
+const caches: Cache[] = [];
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM_POSITION,
+  zoom: ZOOM_LEVEL,
+  minZoom: ZOOM_LEVEL,
+  maxZoom: ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: ZOOM_LEVEL,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+const playerMarker = leaflet.marker(OAKES_CLASSROOM_POSITION);
+playerMarker.bindTooltip(`Your position`);
+playerMarker.addTo(map);
+
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No coins yet...";
+
+function spawnCache(x: number, y: number): Cache {
+  const currentCache: Cache = {
+    x,
+    y,
+    value: Math.floor(luck([x, y, "initialValue"].toString()) * MAX_COINS),
+  };
+  const origin = OAKES_CLASSROOM_POSITION;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + x * TILE_DEGREES, origin.lng + y * TILE_DEGREES],
+    [origin.lat + (x + 1) * TILE_DEGREES, origin.lng + (y + 1) * TILE_DEGREES],
+  ]);
+
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  rect.bindPopup(() => {
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at (${x},${y}). It has <span id="value">${currentCache.value}</span> coins.</div>`;
+    return popupDiv;
+  });
+  return currentCache;
+}
+
+for (let x = -NEIGHBORHOOD_SIZE; x < NEIGHBORHOOD_SIZE; x++) {
+  for (let y = -NEIGHBORHOOD_SIZE; y < NEIGHBORHOOD_SIZE; y++) {
+    if (luck([x, y].toString()) < CACHE_SPAWN_PROBABILITY) {
+      caches.push(spawnCache(x, y));
+    }
+  }
+}
