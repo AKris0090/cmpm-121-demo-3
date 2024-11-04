235e63bfabc7355fcf98ef4a1325917fe21fa5c8
Arjun Krishnan <akrish29@ucsc.edu>
2024-11-03 22:48:30 -0800

    L16 Dialogue

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+const button = document.createElement("button");
+button.innerHTML = "Click me";
+
+document.body.appendChild(button);
+
+button.onclick = () => {
+  alert("you clicked the button!");
+};
